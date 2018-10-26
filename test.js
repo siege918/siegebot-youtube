1f6d237adb8c74f51b2f960d3106862a494c1311
siege918 <mebecjalt@gmail.com>
2018-10-25 22:54:46 -0700

    Formatting changes

--- test.js
+++ test.js
@@ -5,36 +5,36 @@ var mockMessage = {
     author: "@TESTUSER",
     content: ":youtube BoringTrousers My Life Me",
     channel: {
-        send: function(message) { return true; }
+        send: function (message) { return true; }
     }
 };
 
 var key = process.argv[2];
 
-youtube(mockMessage, {youtube: {key: key }}, function(response) {
+youtube(mockMessage, { youtube: { key: key } }, function (response) {
     assert.strictEqual(response, "@TESTUSER, https://www.youtube.com/watch?v=WMbixFBa2M4", "Default success message not populating correctly");
 });
 
 youtube(
-    Object.assign({}, mockMessage, {content: ":youtube habababalagarabagatatatara"}),
-    {youtube: {key: key }},
-    function(response) {
+    Object.assign({}, mockMessage, { content: ":youtube habababalagarabagatatatara" }),
+    { youtube: { key: key } },
+    function (response) {
         assert.strictEqual(response, "Sorry, @TESTUSER, I couldn't find a video for that...", "Default failure message not populating correctly");
     }
 );
 
 youtube(
     mockMessage,
-    {youtube: {key: key, successMessage: "{mention} {mention} {mention} {videoLink} {videoLink} {mention}"}},
-    function(response) {
+    { youtube: { key: key, successMessage: "{mention} {mention} {mention} {videoLink} {videoLink} {mention}" } },
+    function (response) {
         assert.strictEqual(response, "@TESTUSER @TESTUSER @TESTUSER https://www.youtube.com/watch?v=WMbixFBa2M4 https://www.youtube.com/watch?v=WMbixFBa2M4 @TESTUSER", "Custom success message not populating correctly");
     }
 );
 
 youtube(
-    Object.assign({}, mockMessage, {content: ":youtube habababalagarabagatatatara"}),
-    {youtube: {key: key, failureMessage: "{mention} {mention} {mention} {mention}" }},
-    function(response) {
+    Object.assign({}, mockMessage, { content: ":youtube habababalagarabagatatatara" }),
+    { youtube: { key: key, failureMessage: "{mention} {mention} {mention} {mention}" } },
+    function (response) {
         assert.strictEqual(response, "@TESTUSER @TESTUSER @TESTUSER @TESTUSER", "Custom failure message not populating correctly");
     }
 );
